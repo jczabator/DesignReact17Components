e8b3250db4874cb16da41a30fded4fbc914a2e29
Jakub Czabator <jczabator@gmail.com>
2022-12-29 00:40:40 +0100

    fix typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
             <img alt="SVCC Home Page" src="/images/SVCCLogo.png" />
           </div>
           <div className="light">
-            <h4 className="header-title">Silicon Valey Code Camp</h4>
+            <h4 className="header-title">Silicon Valley Code Camp</h4>
           </div>
           <div className={theme === "light" ? "" : "text-info"}>
             Hello Mr. Smith &nbsp;&nbsp;
